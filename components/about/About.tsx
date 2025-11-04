756745d631066eb22f10cce55ca8ea8017be3f4f
David Nienge <davidnienge@gmail.com>
2025-11-04 19:12:53 +0100

    Update About.tsx

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -25,12 +25,12 @@ export const About: React.FC = () => {
                 <div>
                     <p>
                         {
-                            'Programming is both my profession and my hobby, which is why I truly enjoy my work as a developer. With over 3 years of experience, I have the skills to tackle a wide range of challenges, from building interactive web applications to leading development teams.'
+                            'Programming is both my profession and my hobby, which is why I truly enjoy my work as a developer. With over 4 years of experience, I have the skills to tackle a wide range of challenges, from building interactive web applications to leading development teams.'
                         }
                     </p>
                     <p>
                         {
-                            'I am currently an undergrad student at Mewer International University Abuja majoring in Software Engineering. I volunteer at WOMOC (World Media Outreach Center) as a software engineer(remote) and previously, I worked at eHealth Africa and the presidency(FGNPC), both as a fullstack software engineer. Outside work, I spend my time gaming, taking long naps, and sharing algorithms/discoveries on Instagram.'
+                            'I currently work as the lead software engineer at Syntax Labs. I volunteer at WOMOC (World Media Outreach Center) as a software engineer(remote) and previously, I worked at eHealth Africa and the presidency(FGNPC), both as a fullstack software engineer. Outside work, I spend my time gaming, taking long naps, and sharing algorithms/discoveries on Instagram.'
                         }
                     </p>
                 </div>
